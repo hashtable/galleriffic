785e2581a7e9132de6b74ec148ef982a32ea9369
Carsten Milling <cmil@hashtable.de>
2012-07-08 00:05:23 +0200

    untabified and re-indented

--- example/js/jquery.opacityrollover.js
+++ example/js/jquery.opacityrollover.js
@@ -6,37 +6,37 @@
  *   http://www.opensource.org/licenses/mit-license.php
  */
 ;(function($) {
-	var defaults = {
-		mouseOutOpacity:   0.67,
-		mouseOverOpacity:  1.0,
-		fadeSpeed:         'fast',
-		exemptionSelector: '.selected'
-	};
+    var defaults = {
+        mouseOutOpacity:   0.67,
+        mouseOverOpacity:  1.0,
+        fadeSpeed:         'fast',
+        exemptionSelector: '.selected'
+    };
 
-	$.fn.opacityrollover = function(settings) {
-		// Initialize the effect
-		$.extend(this, defaults, settings);
+    $.fn.opacityrollover = function(settings) {
+        // Initialize the effect
+        $.extend(this, defaults, settings);
 
-		var config = this;
+        var config = this;
 
-		function fadeTo(element, opacity) {
-			var $target = $(element);
+        function fadeTo(element, opacity) {
+            var $target = $(element);
 
-			if (config.exemptionSelector)
-				$target = $target.not(config.exemptionSelector);
+            if (config.exemptionSelector)
+                $target = $target.not(config.exemptionSelector);
 
-			$target.fadeTo(config.fadeSpeed, opacity);
-		}
+            $target.fadeTo(config.fadeSpeed, opacity);
+        }
 
-		this.css('opacity', this.mouseOutOpacity)
-			.hover(
-				function () {
-					fadeTo(this, config.mouseOverOpacity);
-				},
-				function () {
-					fadeTo(this, config.mouseOutOpacity);
-				});
+        this.css('opacity', this.mouseOutOpacity)
+            .hover(
+                function () {
+                    fadeTo(this, config.mouseOverOpacity);
+                },
+                function () {
+                    fadeTo(this, config.mouseOutOpacity);
+                });
 
-		return this;
-	};
+        return this;
+    };
 })(jQuery);
